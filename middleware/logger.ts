165d37c41a04e63140c7b8c43bb753980b55ead5
Trevor-akshay <trevorakshay@gmail.com>
2025-12-22 20:20:33 -0500

    logging middleware

---
+++ middleware/logger.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+
+const logger = (req: Request, _res: Response, next: NextFunction) => {
+  console.log(`Method: ${req.method} , URL: ${req.url}`);
+  next();
+};
+
+export default logger;
