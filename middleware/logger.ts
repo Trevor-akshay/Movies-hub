0567272622ea2416a3a5e1e068ed7514baa64512
Trevor-akshay <trevorakshay@gmail.com>
2025-12-27 19:07:48 -0500

    logger

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const logger = (req: Request, _res: Response, next: NextFunction) => {
   console.log(`Method: ${req.method} , URL: ${req.url}`);
-  next();
+  next()
 };
 
 export default logger;
